348981ff4e054c0d55713b7e6f1aefeabad65a3e
Casey Erbes <casey@erbes.com>
2019-02-04 22:46:09 -0600

    Adding source directory detection in env.sh

--- env.sh
+++ env.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 export PATH="$PATH:$(go env GOPATH)/bin"
-export GOPATH="$(go env GOPATH):$(pwd)"
-export GOBIN="$(pwd)/bin"
+export GOPATH="$(go env GOPATH):$DIR"
+export GOBIN="$DIR/bin"
 
 export DBHOST=localhost
 export DBPORT=5432
