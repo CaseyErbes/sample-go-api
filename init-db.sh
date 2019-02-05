9ec65b22a5cecb7efc6c5cf7b1a597452dec0649
erbes <erbes@ubuntu.localdomain>
2019-02-05 04:09:44 +0000

    Fixing init-db.sh

--- init-db.sh
+++ init-db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /
-psql postgres -c "DROP DATABASE IF EXISTS postgres_db;"
-psql postgres -c "DROP DATABASE IF EXISTS postgres_test_db;"
-psql postgres -c "CREATE DATABASE postgres_db;"
-psql postgres -c "CREATE DATABASE postgres_test_db;"
-psql postgres -c "CREATE EXTENSION \"uuid-ossp\";" -d postgres_db
-psql postgres -c "CREATE EXTENSION \"uuid-ossp\";" -d postgres_test_db
+sudo -u postgres psql -c "DROP DATABASE IF EXISTS postgres_db;"
+sudo -u postgres psql -c"DROP DATABASE IF EXISTS postgres_test_db;"
+sudo -u postgres psql -c "CREATE DATABASE postgres_db;"
+sudo -u postgres psql -c "CREATE DATABASE postgres_test_db;"
+sudo -u postgres psql -c "CREATE EXTENSION \"uuid-ossp\";" -d postgres_db
+sudo -u postgres psql -c "CREATE EXTENSION \"uuid-ossp\";" -d postgres_test_db
 cd -
